de01653e09102fbf1deb6416752bef60742afc67
cryptogopher <git6453hub@michalczyk.pro>
2020-10-06 23:13:58 +0200

    Added plugin configure, plugin free_data

--- panel-plugin/alarm.c
+++ panel-plugin/alarm.c
@@ -42,12 +42,50 @@ struct _AlarmPlugin
 
 
 static void
+plugin_construct(XfcePanelPlugin *panel_plugin)
+{
+  AlarmPlugin *plugin = XFCE_ALARM_PLUGIN(panel_plugin);
+
+  xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
+  xfce_panel_plugin_menu_show_configure (panel_plugin);
+  xfce_panel_plugin_set_small (panel_plugin, TRUE);
+
+  gtk_widget_show(plugin->panel_button);
+}
+
+static void
+plugin_free_data(XfcePanelPlugin *panel_plugin)
+{
+  //AlarmPlugin *plugin = XFCE_ALARM_PLUGIN(panel_plugin);
+}
+
+static void
+plugin_configure(XfcePanelPlugin *panel_plugin)
+{
+}
+
+
+static gboolean
+panel_size_changed(XfcePanelPlugin *panel_plugin, gint size)
+{
+  size /= xfce_panel_plugin_get_nrows(panel_plugin);
+  if (xfce_panel_plugin_get_orientation(panel_plugin) == GTK_ORIENTATION_HORIZONTAL)
+    gtk_widget_set_size_request(GTK_WIDGET(panel_plugin), -1, size);
+  else
+    gtk_widget_set_size_request(GTK_WIDGET(panel_plugin), size, -1);
+
+  return TRUE;
+}
+
+static void
 panel_orientation_changed(XfcePanelPlugin *panel_plugin, GtkOrientation orientation)
 {
   AlarmPlugin *plugin = XFCE_ALARM_PLUGIN(panel_plugin);
   GtkWidget *box = gtk_bin_get_child(GTK_BIN(plugin->panel_button));
 
   gtk_orientable_set_orientation(GTK_ORIENTABLE(box), orientation);
+
+  panel_size_changed(panel_plugin, xfce_panel_plugin_get_size(panel_plugin));
 }
 
 static void
@@ -59,20 +97,6 @@ panel_button_toggled(GtkWidget *panel_button, AlarmPlugin *plugin)
   //xfce_panel_plugin_block_autohide(XFCE_PANEL_PLUGIN(plugin), TRUE);
 }
 
-
-static void
-alarm_plugin_construct (XfcePanelPlugin *panel_plugin)
-{
-  AlarmPlugin *plugin = XFCE_ALARM_PLUGIN(panel_plugin);
-
-  xfce_textdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR, "UTF-8");
-
-  xfce_panel_plugin_menu_show_configure (panel_plugin);
-
-  xfce_panel_plugin_set_small (panel_plugin, TRUE);
-
-  gtk_widget_show(plugin->panel_button);
-}
 
 
 /* define the plugin */
@@ -89,12 +113,12 @@ alarm_plugin_class_init(AlarmPluginClass *klass)
   //gobject_class->set_property = directory_menu_plugin_set_property;
 
   plugin_class = XFCE_PANEL_PLUGIN_CLASS(klass);
-  plugin_class->construct = alarm_plugin_construct;
-  //plugin_class->free_data = directory_menu_plugin_free_data;
-  //plugin_class->size_changed = directory_menu_plugin_size_changed;
+  plugin_class->construct = plugin_construct;
+  plugin_class->free_data = plugin_free_data;
+  plugin_class->configure_plugin = plugin_configure;
+  plugin_class->size_changed = panel_size_changed;
   plugin_class->orientation_changed = panel_orientation_changed;
-  //plugin_class->configure_plugin = directory_menu_plugin_configure_plugin;
-  //plugin_class->remote_event = directory_menu_plugin_remote_event;
+  //plugin_class->remote_event = plugin_remote_event;
 }
 
 static void
@@ -109,106 +133,9 @@ alarm_plugin_init(AlarmPlugin *plugin)
   g_signal_connect(G_OBJECT(plugin->panel_button), "toggled",
                    G_CALLBACK(panel_button_toggled), plugin);
 
-  // TODO: check if orientation-changed signal is emitted on init and update if not
+  // TODO: check if orientation-changed signal is emitted on init
+  // and call orientation_changed if necessary
   widget = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
   gtk_container_add(GTK_CONTAINER(plugin->panel_button), widget);
   gtk_box_pack_start(GTK_BOX(widget), gtk_progress_bar_new(), TRUE, FALSE, 0);
 }
-
-/*
-static SamplePlugin *
-sample_new (XfcePanelPlugin *plugin)
-{
-  SamplePlugin   *sample;
-  GtkOrientation  orientation;
-  GtkWidget      *label;
-
-  // allocate memory for the plugin structure
-  sample = g_slice_new0 (SamplePlugin);
-
-  // pointer to plugin
-  sample->plugin = plugin;
-
-  // read the user settings
-  sample_read (sample);
-
-  // get the current orientation
-  orientation = xfce_panel_plugin_get_orientation (plugin);
-
-  // create some panel widgets
-  sample->ebox = gtk_event_box_new ();
-  gtk_widget_show (sample->ebox);
-
-  sample->hvbox = gtk_box_new (orientation, 2);
-  gtk_widget_show (sample->hvbox);
-  gtk_container_add (GTK_CONTAINER (sample->ebox), sample->hvbox);
-
-  // some sample widgets
-  label = gtk_label_new (_("Sample"));
-  gtk_widget_show (label);
-  gtk_box_pack_start (GTK_BOX (sample->hvbox), label, FALSE, FALSE, 0);
-
-  label = gtk_label_new (_("Plugin"));
-  gtk_widget_show (label);
-  gtk_box_pack_start (GTK_BOX (sample->hvbox), label, FALSE, FALSE, 0);
-
-  return sample;
-}
-
-
-
-static void
-sample_free (XfcePanelPlugin *plugin,
-             SamplePlugin    *sample)
-{
-  GtkWidget *dialog;
-
-  // check if the dialog is still open. if so, destroy it
-  dialog = g_object_get_data (G_OBJECT (plugin), "dialog");
-  if (G_UNLIKELY (dialog != NULL))
-    gtk_widget_destroy (dialog);
-
-  // destroy the panel widgets
-  gtk_widget_destroy (sample->hvbox);
-
-  // cleanup the settings
-  if (G_LIKELY (sample->setting1 != NULL))
-    g_free (sample->setting1);
-
-  // free the plugin structure
-  g_slice_free (SamplePlugin, sample);
-}
-
-
-
-static void
-sample_orientation_changed (XfcePanelPlugin *plugin,
-                            GtkOrientation   orientation,
-                            SamplePlugin    *sample)
-{
-  // change the orientation of the box
-  gtk_orientable_set_orientation(GTK_ORIENTABLE(sample->hvbox), orientation);
-}
-
-
-
-static gboolean
-sample_size_changed (XfcePanelPlugin *plugin,
-                     gint             size,
-                     SamplePlugin    *sample)
-{
-  GtkOrientation orientation;
-
-  // get the orientation of the plugin
-  orientation = xfce_panel_plugin_get_orientation (plugin);
-
-  // set the widget size
-  if (orientation == GTK_ORIENTATION_HORIZONTAL)
-    gtk_widget_set_size_request (GTK_WIDGET (plugin), -1, size);
-  else
-    gtk_widget_set_size_request (GTK_WIDGET (plugin), size, -1);
-
-  // we handled the orientation
-  return TRUE;
-}
-*/
